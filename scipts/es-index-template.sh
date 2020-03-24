3d9429e574fc087dbe6ffedf45fe1245ba76ada3
Lorenzo Aiello <lorenzo@aiello.family>
2020-03-24 03:21:17 -0700

    moved scripts to sub-folder
    Signed-off-by: Lorenzo Aiello <lorenzo@aiello.family>

---
+++ scipts/es-index-template.sh
@@ -0,0 +1,88 @@
+curl -XPUT -H 'Content-Type: application/json' "${ES_HOST:-localhost}:${ES_PORT:-9200}/_template/statsd-template" -d '
+PUT /_template/statsd-template
+{
+    "index_patterns" : ["statsd-*"],
+    "settings" : {
+        "number_of_shards" : 1
+    },
+    "mappings" : {
+        "_source" : { "enabled" : true },
+        "properties" : {
+            "type": {
+                "type": "keyword"
+            },
+            "@timestamp": {
+                "type": "date"
+            },
+            "val": {
+                "type": "double",
+                "index": "true"
+            },
+            "name": {
+                "type": "keyword",
+                "index": "true"
+            },
+            "count_ps": {
+                "type": "float",
+                "index": "true"
+            },
+            "count": {
+                "type": "float",
+                "index": "true"
+            },
+            "upper": {
+                "type": "float",
+                "index": "true"
+            },
+            "lower": {
+                "type": "float",
+                "index": "true"
+            },
+            "mean": {
+                "type": "float",
+                "index": "true"
+            },
+            "median": {
+                "type": "float",
+                "index": "true"
+            },
+            "std": {
+                "type": "float",
+                "index": "true"
+            },
+             "sum": {
+                "type": "float",
+                "index": "true"
+            },
+            "mean_90": {
+                "type": "float",
+                "index": "true"
+            },
+            "upper_90": {
+                "type": "float",
+                "index": "true"
+            },
+            "sum_90": {
+                "type": "float",
+                "index": "true"
+            },
+            "bin_100": {
+                "type": "integer",
+                "index": "true"
+            },
+            "bin_500": {
+                "type": "integer",
+                "index": "true"
+            },
+            "bin_1000": {
+                "type": "integer",
+                "index": "true"
+            },
+            "bin_inf": {
+                "type": "integer",
+                "index": "true"
+            }
+        }
+    }
+}
+```
